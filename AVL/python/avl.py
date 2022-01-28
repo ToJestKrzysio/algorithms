54f59cc8d2c077c7308dd3a3593fdfd1d44ddd0d
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-01-28 16:10:15 +0100

    working binary tree with representation

--- AVL/python/avl.py
+++ AVL/python/avl.py
@@ -3,119 +3,87 @@ from __future__ import annotations
 from typing import Optional
 
 
-class Node:
-    value: Optional[Node]
-    left: Optional[Node]
-    right: Optional[Node]
-    parent: Optional[Node]
-    height: int  # negative left heavy, positive right heavy
+class Tree:
+    root: Node
 
     def __init__(self):
-        self.height = -1
+        self.root = Node()
+
+    def __str__(self, *args, **kwargs):
+        return self.root.__str__(*args, **kwargs)
+
+    @classmethod
+    def build(cls, array: list) -> Tree:
+        tree = cls()
+        for value in array:
+            tree.root.insert(value)
+        return tree
+
+
+class Node:
+    value: Optional[int]
+    left: Optional[Node]
+    right: Optional[Node]
+    height: int
+
+    def __init__(self):
         self.value = None
         self.left = None
         self.right = None
-        self.parent = None
-        self.heavy = 0
+        self.height = -1
 
-    def insert(self, value):
-        is_valid = self._insert(value)
-        self.update_height()
-        while not self.check_avl_property():
-            if self.heavy > 1:
-                if self.heavy * self.right.heavy < 0:
-                    self.right.right_rotate()
-                self.right.left_rotate()
-            else:
-                if self.heavy * self.right.heavy > 0:
-                    self.left.left_rotate()
-                self.left.right_rotate()
-        return is_valid
-
-    def _insert(self, value) -> bool:
+    def insert(self, value: int) -> None:
         if self.value is None:
             self.value = value
             self.left = Node()
-            self.left.parent = self
             self.right = Node()
-            self.right.parent = self
-            return True
-        if value < self.value:
+        elif value < self.value:
             self.left.insert(value)
-            return True
-        self.right.insert(value)
-        return True
+        else:
+            self.right.insert(value)
+        self.update_height()
 
-    def right_rotate(self):
-        current_parent = self.parent
-        current_node = self
-        new_node = self.left
-
-        current_parent.update_child(current_node, new_node)
-
-        current_node.left = Node()
-        current_node.parent = new_node
-        current_node.update_height()
-
-        new_node.right = current_node
-        new_node.parent = current_parent
-        new_node.update_height()
-        return new_node
-
-    def left_rotate(self):
-        current_parent = self.parent
-        current_node = self
-        new_node = self.right
-
-        current_parent.update_child(current_node, new_node)
-
-        current_node.right = Node()
-        current_node.update_height()
-        current_node.parent = new_node
-
-        new_node.left = current_node
-        new_node.parent = current_parent
-        new_node.update_height()
-
-    def update_child(self, current_child, new_child):
-        if self.left is current_child:
-            self.left = new_child
-        self.right = new_child
-
-    def update_height(self):
+    def update_height(self) -> None:
         self.height = max(self.left.height, self.right.height) + 1
-        self.heavy = self.right.height - self.left.height
 
-    def check_avl_property(self) -> bool:
-        return self.heavy in range(-1, 2)
+    def __bool__(self):
+        return self.value is not None
 
-    def __str__(self):
-        lines, line = [], []
-        to_visit = [self]
-        current_height = self.height
-        while to_visit:
-            visited = to_visit.pop(0)
-            if visited.left is not None:
-                to_visit.append(visited.left)
-            if visited.right is not None:
-                to_visit.append(visited.right)
-            spacer = " " * int(2 ** visited.height - 1)
-            if current_height != visited.height:
-                lines.append(" ".join(line))
-                line = []
-                current_height = visited.height
-            value = " " if visited.value is None else visited.value
-            line.append(f"{spacer}{value}{spacer}")
-        return "\n".join(lines)
+    def __str__(self, spacer_width=0, recursive=False):
+        if recursive is False:
+            lines, empty = spacer_width // 2, spacer_width // 2 + spacer_width % 2
+            if self.value:
+                left_spacer = " " * empty + "_" * lines
+                right_spacer = "_" * lines + " " * empty
+                value = str(self.value)
+            else:
+                left_spacer = right_spacer = " " * spacer_width
+                value = " "
+            return f"{left_spacer}{value}{right_spacer}"
 
-    @classmethod
-    def build(cls, array: list) -> Node:
-        node = Node()
-        for value in array:
-            node.insert(value)
-        return node
+        depth = 0
+        max_height = self.height
+        current_row = [self]
+        lines = []
+        while current_row:
+            next_row = []
+            finish = True
+            line = ""
+            spacer_width = int(2 ** (max_height - depth) - 1)
+            for visited in current_row:
+                line += visited.__str__(spacer_width=spacer_width) + " "
+                next_row.append(visited.left if visited.left is not None else Node())
+                next_row.append(visited.right if visited.right is not None else Node())
+                if visited:
+                    finish = False
+            depth += 1
+            lines.append(line)
+            current_row = next_row
+            if finish:
+                break
+        return "\n".join(lines[0:-1])
 
 
 if __name__ == '__main__':
-    tree = Node.build([5, 3, 1])
-    print(tree.left.left.value)
+    tree = Tree.build([5, 3, 7, 1, 8, 4, 9, 5, 6, 0, 8, 4, 3, 6, 2, 1, 8])
+    print(tree.__str__(recursive=True))
